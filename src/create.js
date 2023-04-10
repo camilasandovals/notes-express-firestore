f652fb2e29d8de07ffad2b84415b07173356fe69
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-09 22:00:43 -0400

    ADD create function

---
+++ src/create.js
@@ -0,0 +1,12 @@
+import { db } from "../dbConnect.js"
+import { FieldValue } from "firebase-admin/firestore";
+const coll = db.collection("employees");
+
+
+export async function addEmploye(req, res) {
+  const newEmployee = req.body;
+  //insert timestamp(2/2)
+  newEmployee.createdAt = FieldValue.serverTimestamp()
+  await coll.add(newEmployee);
+  res.status(201).send({ message: "Employee successfully added." });
+}
